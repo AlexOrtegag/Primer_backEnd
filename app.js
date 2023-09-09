e3add926c567fb966e2770a60e527af3391e3d16
Alex <al150303@alumnos.uacj.mx>
2023-09-08 18:37:25 -0600

    inicial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function moverPosicionRandom(elm){
+    elm.style.position = 'absolute';
+    elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
+    elm.style.left = Math.random() * (window.innerWidth - elm.offsetWidth) + 'px';
+}
+
+let btnSi = document.getElementById("btn_si");
+let btnNo = document.getElementById("btn_no");
+let divModoSexo = document.getElementsByClassName("modo_sexo")[0];
+
+btnNo.addEventListener('mouseenter', function(e){moverPosicionRandom(e.target) });//boton no se muevde de manera random
+
+btnSi.addEventListener('click', function(e){
+    alert('sabia que dirias que si, casemonos y tengamos hijos ')
+
+    divModoSexo.style.display = 'block';
+    const cancion = new Audio ('.mp3');//ruta del audio
+    cancion.play();
+});
+
